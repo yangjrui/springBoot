6c159304d894122da0cf9b3f82e2ae8f4f742ccd
yangjrui <18434366551@163.com>
2020-10-22 11:35:49 +0800

    helloSpringBoot

---
+++ src/main/java/com/example/demospringboot/controller/TestController.java
@@ -0,0 +1,13 @@
+package com.example.demospringboot.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+    @RequestMapping("/hello")
+    public String hello(){
+        System.out.println("hello world");
+        return "hello world";
+    }
+}
